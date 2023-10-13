0da5020dbbc5028aee3893332bd0b4cfcca845ca
junkai@ALPHACO.DEV <junkai@ALPHACO.DEV>
2023-10-13 18:52:25 +0800

    Edit, Update, Delete todos

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -11,24 +11,118 @@ import cn from 'classnames'
 import { motion } from 'framer-motion'
 
 export const TodoItem = (props: { todo: Todo }) => {
-  const {todo} = props
+  const { todo } = props
 
-  return(
+  const [editingTodoText, setEditingTodotext] = useState<string>('')
+  const [editingTodoId, setEditingTodoId] = useState<string | null>(null)
+
+  const { deleteTodo, editTodo, updateTodoStatus } = useTodo()
+
+  const editInputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    if (editingTodoId !== null && editInputRef.current) {
+      editInputRef.current.focus()
+    }
+  }, [editingTodoId])
+
+  const handleEdit = (todoId: string, todoText: string) => {
+    setEditingTodoId(todoId)
+    setEditingTodotext(todoText)
+
+    if (editInputRef.current) {
+      editInputRef.current.focus()
+    }
+  }
+
+  const handleUpdate = (todoId: string) => {
+    if (editingTodoText.trim() !== '') {
+      editTodo(todoId, editingTodoText)
+      setEditingTodoId(null)
+      setEditingTodotext('')
+      toast.success('Todo updated successfully!')
+    } else {
+      toast.error('Todo field cannot be empty!')
+    }
+  }
+
+  const handleDelete = (todoId: string) => {
+    deleteTodo(todoId)
+    toast.success('Todo deleted successfuly!')
+  }
+
+  const handleStatusUpdate = (todoId: string) => {
+    updateTodoStatus(todoId)
+    toast.success('Todo status updated successfully!')
+  }
+
+  return (
     <motion.li
       layout
+      key={todo.id}
       className={cn(
         'p-5 rounded-xl bg-zinc-900',
         todo.status === 'completed' && 'bg-opacity-50 text-zinc-500',
       )}
     >
-      <motion.span
-        layout
-        style={{
-          textDecoration: todo.status === 'completed' ? 'line-through' : 'none',
-        }}
-      >
-        {todo.text}
-      </motion.span>
+      {editingTodoId === todo.id ? (
+        <motion.div layout className="flex gap-2">
+          <Input
+            ref={editInputRef}
+            type="text"
+            value={editingTodoText}
+            onChange={e => setEditingTodotext(e.target.value)}
+          />
+          <button
+            className="px-5 py-2 text-sm font-normal text-orange-300 bg-orange-900 border-2 border-orange-900 active:scale-95 rounded-xl"
+            onClick={() => handleUpdate(todo.id)}
+          >
+            Update
+          </button>
+        </motion.div>
+      ) : (
+        <div className='flex flex-col gap-5'>
+          <motion.span
+            layout
+            style={{
+              textDecoration: todo.status === 'completed' ? 'line-through' : 'none',
+            }}
+          >
+            {todo.text}
+          </motion.span>
+          <div className="flex justify-between gap-5 text-white">
+            <button onClick={() => handleStatusUpdate(todo.id)}>
+              {todo.status === 'undone' ? (
+                <span className="flex items-center gap-1">
+                  <BsCheck2Square />
+                  Mark Completed
+                </span>
+              ) : (
+                <span className="flex items-center gap-1">
+                  <TbRefresh />
+                  Mark Undone
+                </span>
+              )}
+            </button>
+            <div className="flex items-center gap-2">
+              <button
+                onClick={() => handleEdit(todo.id, todo.text)}
+                className='flex items-center gap-1'
+              >
+                <FaRegEdit />
+                Edit
+              </button>
+              <button
+                onClick={() => handleDelete(todo.id)}
+                className="flex items-center gap-1 ml-2 text-red-500"
+              >
+                <RiDeleteBin7Line />
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </motion.li>
   )
 }
