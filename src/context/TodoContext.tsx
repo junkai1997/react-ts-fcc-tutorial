7cbf0d5d3de5df2be54491a42ae79e316e94c14e
junkai@ALPHACO.DEV <junkai@ALPHACO.DEV>
2023-10-13 16:43:39 +0800

    new Todo Interface

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -9,21 +9,27 @@ export interface Todo {
 }
 
 interface TodoContextProps {
-  todos: string[]
+  todos: Todo[]
   addTodo: (text: string) => void
 }
 
 export const TodoContext = createContext<TodoContextProps | undefined>(undefined)
 
 export const TodoProvider = (props: { children: React.ReactNode }) => {
-  const [todos, setTodos] = useState<string[]>([])
+  const [todos, setTodos] = useState<Todo[]>([])
 
   const addTodo = (text: string) => {
-    setTodos([...todos, text])
+    const newTodo: Todo = {
+      id: nanoid(),
+      text,
+      status: 'undone',
+    }
+
+    setTodos([...todos, newTodo])
   }
 
   const value: TodoContextProps = {
-    todos, 
+    todos,
     addTodo
   }
 
